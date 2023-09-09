fe9955510e772f1947819d035ccc7fb99dc6a8b5
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-09-09 14:53:57 +0900

    second commit

--- src/main/java/com/example/cicd/Controller/MemberController.java
+++ src/main/java/com/example/cicd/Controller/MemberController.java
@@ -1,35 +1,16 @@
 package com.example.cicd.Controller;
 
-import com.example.cicd.Model.DAO.Member;
-import com.example.cicd.Model.DTO.MemberDTO;
-import com.example.cicd.Model.DTO.MemberPostDTO;
-import com.example.cicd.Repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
-    @Autowired
-    private MemberRepository memberRepository;
-
     @GetMapping
-    public ResponseEntity<List<MemberDTO>> getMembers() {
-        List<Member> memberList = memberRepository.findAll();
-        List<MemberDTO> memberDTOS = memberList.stream().map(MemberDTO::of).collect(Collectors.toList());
-        return ResponseEntity.ok(memberDTOS);
-    }
+    public String getMembers() {
 
-    @PostMapping
-    public ResponseEntity<MemberDTO> createMember(@RequestBody MemberPostDTO memberPostDTO) {
-        Member member = new Member();
-        member.setName(memberPostDTO.getName());
-        memberRepository.save(member);
-        return ResponseEntity.ok(MemberDTO.of(member));
+        return "Hello World 1";
     }
 }
--- src/main/java/com/example/cicd/Repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cicd.Repository;
-
-
-import com.example.cicd.Model.DAO.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
